9464c760c0a4b6f99e9957f2d20f6dacdc39f5a9
Keila Lopez <keilalofra@gmail.com>
2023-01-10 10:21:14 -0600

    Updated sessions controller to reflect changes needed after adding rescue from. Removed ifelse statement.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,10 @@
 class SessionsController < ApplicationController
 
-    def login
+    def create
         # Using find by bc we are looking by username 
-        user = User.find_by(name: params[:name])
-
-        if user&.authenticate(params[:password])
-            #set user to sessions
-            render json: user, status: :ok
-        else
-            render json: {errors: "Invalid Password or Username"}, status: :unauthorized
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
         end
     end
 
