485963003b6e78352c49b6c7d08a0bf1b859025a
Keila Lopez <keilalofra@gmail.com>
2023-01-12 16:11:25 -0600

    Added About component and linked to landing page

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -1,11 +1,13 @@
 import SignupLoginForms from './SigupLoginForms.js';
 import LoginForm from './LoginForm.js';
+import About from './About.js';
 
 function LandingPage () {
     return (
         <div>
             <SignupLoginForms />
             <LoginForm />
+            <About />
         </div>
     )
 }
