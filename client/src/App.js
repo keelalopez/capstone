0d2f988f47e7ca7e22bee695f587dab61fa540cf
Keila Lopez <keilalofra@gmail.com>
2023-01-13 08:26:30 -0600

    Git merge issues resolved.

--- client/src/App.js
+++ client/src/App.js
@@ -45,27 +45,6 @@ function App() {
     .then(setMaterials)
   }, [])
 
-  // STAY LOGGED IN
-  useEffect(() => {
-    // fetch("/me")
-    // .then(res => {
-    //   if(res.ok){
-    //     res.json().then(user => {setCurrentUser(user)})
-    //   }
-    // })
-  }, [])
-
-  // HANDLE LOG OUT
-  const handleLogOut = () => {
-    fetch("/logout", {
-      method: "DELETE"
-    })
-    .then(res => {
-      if(res.ok){
-        setCurrentUser(null)
-      }
-    })
-  }
 
   return (
    <div className="App">
