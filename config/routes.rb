ca4b5066b46a0a5ba8d7e4bfda8c4f165c9960ec
Keila Lopez <keilalofra@gmail.com>
2023-01-13 08:18:25 -0600

    Updated auth and resolved merging conflicts. Working branch

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # CUSTOM ROUTES 
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 
 
   # Routing logic: fallback requests for React Router.
