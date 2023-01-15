129119609537723e8243bbad14f5aa909f28a62d
Keila Lopez <keilalofra@gmail.com>
2023-01-15 07:58:10 -0700

    Added project buttons and linked to be rerouted

--- client/src/Components/ProjectElement.js
+++ client/src/Components/ProjectElement.js
@@ -1,8 +1,38 @@
+import { Button, Card, Icon} from 'semantic-ui-react'
+import { Link } from 'react-router-dom';
 
 function ProjectElement ({name}) {
+
+
     return (
-        <div>Project {name}</div>
+        <div className="projects">
+            <Card>
+                <Card.Content header={name} />
+                <Card.Content extra>
+                    <Link to="/project">
+                        <Button>Project</Button>
+                    </Link>
+                    <Link to="/materials"> 
+                        <Button>Materials</Button>
+                    </Link>
+                </Card.Content>
+            </Card>
+         </div>
     )
 }
 
 export default ProjectElement;
+
+{/* <Button icon>
+    <Icon name="building" />
+</Button>
+<Button icon>
+    <Icon name="dolly flatbed" />
+</Button> */}
+
+{/* <Button animated>
+    <Button.Content visible>
+        <Icon name='dolly' />
+    </Button.Content>
+    <Button.Content hidden>Materials</Button.Content>
+</Button> */}
