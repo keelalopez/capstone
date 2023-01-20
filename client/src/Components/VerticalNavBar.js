5de93d5298de56830af668175ea1630035087629
Keila Lopez <keilalofra@gmail.com>
2023-01-20 10:44:08 -0600

    New VerticalNavBar component created. Functional side nav bar.

--- client/src/Components/VerticalNavBar.js
+++ client/src/Components/VerticalNavBar.js
@@ -1,15 +1,17 @@
+// import { Link } from 'react-router-dom';
+
 
 function VerticalNavBar () {
     return (
         <div className="vertical-nav">
             <ul className="vertical-nav">
-                <li>Projects</li>
-                <li>Materials</li>
-                <li>Divisions</li>
-                <li>User</li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/materials">Materials</a></li>
+                <li><a href="/divisions">Divisions</a></li>
+                <li><a href="/">User</a></li>
+
             </ul>
         </div>
-
     )
 }
 
